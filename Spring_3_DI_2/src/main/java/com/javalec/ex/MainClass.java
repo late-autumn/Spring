9136bcb3b27977a3a8b84c9e92fbde7e3492b28d
late-autumn <late_autumn88@naver.com>
2017-04-19 15:11:33 +0900

    DI2

---
+++ Spring_3_DI_2/src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,17 @@
+package com.javalec.ex;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		String configLocation = "classpath:applicationCTX.xml";
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext(configLocation);
+		MyInfo myinfo = ctx.getBean("myInfo",MyInfo.class);
+		myinfo.getInfo();
+		ctx.close();
+	
+	}
+	
+}
